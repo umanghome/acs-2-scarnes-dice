f4293e3ee7e8583e1e0fed0bfd1f228af8716410
Umang Galaiya <umanghome@gmail.com>
2016-06-17 01:08:44 +0530

    Modified UI

--- app/src/main/java/com/umanghome/scarnesdice/MainActivity.java
+++ app/src/main/java/com/umanghome/scarnesdice/MainActivity.java
@@ -5,9 +5,27 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Scores
+    private int userScore;
+    private int computerScore;
+
+    // Turn scores
+    private int userTurnScore;
+    private int computerTurnScore;
+
+    // Determine turn (odd = computer, even = user)
+    private int turn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize scores and turns to zero
+        userScore = 0;
+        computerScore = 0;
+        userTurnScore = 0;
+        computerTurnScore = 0;
+        turn = 0;
     }
 }
